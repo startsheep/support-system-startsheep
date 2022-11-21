cf3e10c810e499803e667b4479e920c747a2ed4b
hakim-asrori <saneglos005@gmail.com>
2022-11-21 13:08:29 +0700

    move again

--- database/seeders/TicketSeeder.php
+++ database/seeders/TicketSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\Ticket;
 use Database\Seeders\Traits\DisableForeignKey;
 use Database\Seeders\Traits\TruncateTable;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TicketSeeder extends Seeder
